0ccd804f03c68fb343b42c4f7e8926b7fde9538f
snaranjoa <50253439+snaranjoa@users.noreply.github.com>
2019-05-17 16:12:39 -0500

    Script parcial

--- script.sh
+++ script.sh
@@ -1,5 +1,13 @@
 #Escriba su código aquí
 
+#Eliminar filas en blanco
+head -n 3 data1.csv > datas1.csv
+cat datas1.csv > data1.csv
+tail -n  2 data2.csv > datas2.csv
+cat datas2.csv > data2.csv
+rm datas*.csv
+
 #Crear .txt con lista de archivos en la carpeta
 ls -1 > lista1.txt
 sed -n '/.csv/p' lista1.txt > lista.txt
+
